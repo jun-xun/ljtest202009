db5f8b5f1702afb3e1bd3b6f6d6012bcfe4a9278
俊 巡 <65719973+jun-xun@users.noreply.github.com>
2020-10-13 22:50:32 +0800

    Add files via upload

---
+++ day02-3.py
@@ -0,0 +1,21 @@
+a = "小花花"
+
+# if a== "小花花":
+#     print("我是小花花")
+# else:
+#     print("我不是小花花！")
+
+# age = int(input("请输入你的年龄："))
+# if age > 18:
+#     print("welcome")
+# else:
+#     print("you can come when you are adult")
+
+a = int(input("请输入一个文字："))
+# b = "你好好好好好"
+# c = {"好好好",123}
+# d = {"哈哈哈哈",123}
+if a >18 or a < 25:
+    print("找到了")
+else:
+    print("木有啊！")
